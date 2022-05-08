91c3f12edfdddb112b562de1a81e2dd8b72130c4
xychua <1821656+xychua@users.noreply.github.com>
2022-05-08 15:46:32 +1000

    add required packages

--- Rpackages.R
+++ Rpackages.R
@@ -3,11 +3,14 @@ install.packages(c('argparse',
                    'tidyverse',
                    'funrar',
                    'data.table',
+                   'dunn.test',
                    'reshape2',
                    'vegan',
                    'ape',
                    'ggfortify',
                    'ggpubr',
                    'otuSummary',
-                   'devtools'))
+                   'devtools',
+                   'BiocManager'))
+BiocManager::install('EnhancedVolcano', ask=F)
 devtools::install_github("pmartinezarbizu/pairwiseAdonis/pairwiseAdonis")
